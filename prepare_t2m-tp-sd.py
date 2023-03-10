c4145bf0aa6d03c8e2d6624f360f4455c5bc02f4
j34ni <44261743+j34ni@users.noreply.github.com>
2023-03-10 16:47:09 +0100

    Add files via upload
    Added SD from ERA5, and updated finn.ipynb

--- prepare_t2m-tp-sd.py
+++ prepare_t2m-tp-sd.py
@@ -128,6 +128,8 @@ for Year in range(2015, 2020):
 # Extract ERA5 t2m, tp and sd field 
     ERA5_t2m = ERA5.where(ERA5['latitude'].isin(dvx['ERA5_lat'].values) & ERA5['longitude'].isin(dvx['ERA5_lon'].values))['t2m']
     ERA5_tp = ERA5.where(ERA5['latitude'].isin(dvx['ERA5_lat'].values) & ERA5['longitude'].isin(dvx['ERA5_lon'].values))['tp']
+    ERA5_sd = ERA5.where(ERA5['latitude'].isin(dvx['ERA5_lat'].values) & ERA5['longitude'].isin(dvx['ERA5_lon'].values))['sd']
+    
     print('ERA5_t2m ', ERA5_t2m)
 
 # Rain on Snow criteria (according to https://www.hydrol-earth-syst-sci.net/23/2983/2019/hess-23-2983-2019.pdf)
@@ -138,26 +140,32 @@ for Year in range(2015, 2020):
 # and¶
 # initial snowpack depth of at least 10 cm
 
-# Normalizing temperature and total precipitation values accordidng to these criteria
+# Normalizing temperature, total precipitation and snow depth values accordidng to these criteria
     ERA5_t2m = ERA5_t2m / 273.15
     ERA5_tp = ERA5_tp / 0.02 * 12.
+    ERA5_sd = ERA5_sd / 0.1
 
     df_t2m = ERA5_t2m.stack(z=['latitude', 'longitude']).to_pandas().transpose().reset_index()
     df_tp = ERA5_tp.stack(z=['latitude', 'longitude']).to_pandas().transpose().reset_index()
+    df_sd = ERA5_sd.stack(z=['latitude', 'longitude']).to_pandas().transpose().reset_index()
     df_tp = df_tp.drop(columns=['latitude', 'longitude'])
+    df_sd = df_sd.drop(columns=['latitude', 'longitude'])
 
 # Create labels for ERA5-land variables to replace the dates
     label_t2m = ['latitude', 'longitude']
     label_tp = list()
+    label_sd = list()
     for i in range(Number_of_days * 24):
         label_t2m.append('t2m_'+ str(i))
         label_tp.append('tp_'+ str(i))
+        label_sd.append('sd_'+ str(i))
 
     df_t2m.set_axis(label_t2m, axis="columns", inplace=True)
     df_tp.set_axis(label_tp, axis="columns", inplace=True)
+    df_sd.set_axis(label_sd, axis="columns", inplace=True)
 
-##  Glue together df_t2m and df_tp
-    df = pd.concat([df_t2m, df_tp], axis = 1)
+## Glue together df_t2m, df_tp and df_sd
+    df = pd.concat([df_t2m, df_tp, df_sd], axis = 1)
 
 # Add combined lon_lat column to df
     df['lon_lat'] = (df['longitude'] * 100).astype('int') + (df['latitude'] * 100).astype('int') / 100000
@@ -176,9 +184,9 @@ for Year in range(2015, 2020):
     dwx_pandas = dwx.to_pandas_df()
     dwy_pandas = dwy.to_pandas_df()
 
-## Join dwx (WLC) with df (ERA5 t2m-tp)
+## Join dwx (WLC) with df (ERA5 t2m-tp-sd)
     dx = dwx_pandas.set_index('lon_lat').join(df.set_index('lon_lat'), on='lon_lat')
-    print('dwx (WLC) joined with df (ERA5 t2m-tp)')
+    print('dwx (WLC) joined with df (ERA5 t2m-tp-sd)')
 
 # Drop the Rows with NaN Values
     dx = dx.dropna()
